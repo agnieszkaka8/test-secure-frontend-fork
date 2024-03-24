557b082940152c618c933fc3f37095498e1f8e12
a.kapluk <a.kapluk@gidcoders.pl>
2024-03-24 14:21:49 +0100

    percy demo

--- cypress/e2e/isolation/home.isolated.cy.ts
+++ cypress/e2e/isolation/home.isolated.cy.ts
@@ -1,61 +1,71 @@
 /// <reference types="cypress" />
 
-import { getLoginResponseFor } from "../../domain/http/login"
-import { getRandomUser } from "../../generators/userGenerator"
-import users from "../../fixtures/users.json"
+import { getLoginResponseFor } from '../../domain/http/login';
+import { getRandomUser } from '../../generators/userGenerator';
+import users from '../../fixtures/users.json';
 
 describe('Home page tests', () => {
-    beforeEach(() => {
-        // 1. Zachowuje sobie odpowiedź w localStorage pod kluczem user
-        // 2. Ustawia ciastko token ze zwróconym tokenem
+  beforeEach(() => {
+    // 1. Zachowuje sobie odpowiedź w localStorage pod kluczem user
+    // 2. Ustawia ciastko token ze zwróconym tokenem
 
-        const user = getRandomUser()
-        // 1
-        localStorage.setItem('user', JSON.stringify(getLoginResponseFor(user)))
-        // 2
-        cy.setCookie('token', 'fakeToken')
-        cy.intercept('GET', '**/users', { fixture: 'users.json' })
-        cy.visit('http://localhost:8081')
-    })
+    const user = getRandomUser();
+    // 1
+    localStorage.setItem('user', JSON.stringify(getLoginResponseFor(user)));
+    // 2
+    cy.setCookie('token', 'fakeToken');
+    cy.intercept('GET', '**/users', { fixture: 'users.json' });
+    cy.visit('http://localhost:8081');
+  });
 
-    it('should correctly display all users', () => {
-        // then
-        cy.get('li').should('have.length', users.length)
-        cy.get('li').each(($el, i) => {
-            expect($el).to.contain.text(`${users[i].firstName} ${users[i].lastName}`);
-        })
-    })
+  it('should correctly display all users', () => {
+    // then
+    cy.get('li').should('have.length', users.length);
+    cy.get('li').each(($el, i) => {
+      expect($el).to.contain.text(`${users[i].firstName} ${users[i].lastName}`);
+    });
+    cy.percySnapshot();
+  });
 
-    it('should correctly delete an user', () => {
-        // given
-        const index = 1
-        const userToDelete = users[index]
-        cy.intercept('DELETE', `**/users/${userToDelete.username}`, { statusCode: 204 })
+  it('should correctly delete an user', () => {
+    // given
+    const index = 1;
+    const userToDelete = users[index];
+    cy.intercept('DELETE', `**/users/${userToDelete.username}`, {
+      statusCode: 204,
+    });
 
-        // when
-        cy.get('li').eq(index).find('.delete').click()
+    // when
+    cy.get('li').eq(index).find('.delete').click();
 
-        // then
-        cy.get('li').should('have.length', users.length - 1)
-        cy.get('li').contains(`${userToDelete.firstName} ${userToDelete.lastName}`).should('not.exist')
-    })
+    // then
+    cy.get('li').should('have.length', users.length - 1);
+    cy.get('li')
+      .contains(`${userToDelete.firstName} ${userToDelete.lastName}`)
+      .should('not.exist');
+  });
 
-    it('should not delete an user if window confirm rejected', () => {
-        // given
-        Cypress.on('window:confirm', (confirmationText) => {
-            expect(confirmationText).to.eq('Are you sure you wish to delete this item?')
-            return false
-        })
-        const index = 1
-        const userToDelete = users[index]
-        cy.intercept('DELETE', `**/users/${userToDelete.username}`, { statusCode: 204 })
+  it('should not delete an user if window confirm rejected', () => {
+    // given
+    Cypress.on('window:confirm', (confirmationText) => {
+      expect(confirmationText).to.eq(
+        'Are you sure you wish to delete this item?'
+      );
+      return false;
+    });
+    const index = 1;
+    const userToDelete = users[index];
+    cy.intercept('DELETE', `**/users/${userToDelete.username}`, {
+      statusCode: 204,
+    });
 
-        // when
-        cy.get('li').eq(index).find('.delete').click()
+    // when
+    cy.get('li').eq(index).find('.delete').click();
 
-        // then
-        cy.get('li').should('have.length', users.length)
-        cy.get('li').contains(`${userToDelete.firstName} ${userToDelete.lastName}`).should('exist')
-    })
-
-})
+    // then
+    cy.get('li').should('have.length', users.length);
+    cy.get('li')
+      .contains(`${userToDelete.firstName} ${userToDelete.lastName}`)
+      .should('exist');
+  });
+});
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,8 +1,9 @@
-import './commands'
+import './commands';
+import '@percy/cypress';
 
 beforeEach(() => {
-    cy.log('This will run before each test')
-    if (Cypress.env('isMobile')) {
-        cy.viewport('iphone-8')
-    }
-})
+  cy.log('This will run before each test');
+  if (Cypress.env('isMobile')) {
+    cy.viewport('iphone-8');
+  }
+});
